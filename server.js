92cf09e6f20b452cbd87c336653c53b060dae1e9
Saiban Hussein <shhussein@m.marywood.edu>
2021-11-14 11:13:29 -0600

    fixing a bug

--- server.js
+++ server.js
@@ -15,10 +15,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://maurice:pass123@ds227565.mlab.com:27565/heroku_1rh6ds4r", {
-  useNewUrlParser: true,
-});
 
+mongoose.connect("mongodb://localhost/budget", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 // routes
 app.use(require("./routes/api.js"));
 
